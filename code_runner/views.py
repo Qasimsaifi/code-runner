1aceb365a5cbfbb5b48882667b96612008608f70
Qasimsaifi <106295428+Qasimsaifi@users.noreply.github.com>
2023-06-27 00:26:43 +0530

    node runner added

--- code_runner/views.py
+++ code_runner/views.py
@@ -22,9 +22,20 @@ def execute_code(request):
     return Response({'output': output})
 
 def execute_nodejs(code, input_text):
-    # Execute Node.js code and capture the output
-    # Implement code execution logic for Node.js
-    pass
+    # Write the code to a temporary file
+    with open('temp.js', 'w') as file:
+        file.write(code)
+
+    # Execute the code and capture the output
+    try:
+        output = subprocess.check_output(['node', 'temp.js'], stderr=subprocess.STDOUT, input=input_text, timeout=5, universal_newlines=True)
+    except subprocess.CalledProcessError as e:
+        output = e.output
+
+    # Remove the temporary file
+    os.remove('temp.js')
+
+    return output
 
 def execute_python(code, input_text):
     # Convert the input_text to a string
